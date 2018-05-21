91280811778afe7b51f7103d7136b24a46083553
Defessus <defessus.z@gmail.com>
2018-05-21 13:02:35 +0300

    IterUpdate

--- MAIN/ChislMethods/ChislMethods/FindFuncs/IterationFind.cs
+++ MAIN/ChislMethods/ChislMethods/FindFuncs/IterationFind.cs
@@ -9,25 +9,23 @@ namespace ChislMethods.FindFuncs
     {
         public static double Iteration(double Xn,/*Текущий аргумент*/ double eps,/*Точность*/  DelF Func /*Уравнение*/) //Метод последовательных приближений
         {
-            double currX = Xn; //Выбираем текущий Х
-            double FuncX = Func(currX); //Подставляем в уравнение
-            long iteration = 0; //Задаём номер итерации
+            double x = 0;
+            int i = 0;
+            bool error = false;
 
-            var t = Math.Abs(FuncX - currX); //Модуль разницы между F(x) и х
             do
             {
-                currX = FuncX; // Х+1 = F(x) 
-                FuncX = Func(currX); // обновляем значение функции от текущего Х
-                var currentT = Math.Abs(FuncX - currX); // обновляем модуль разницы между F(x) и х
-                if (currentT > t) // если обновленное значение больше предыдущего, то выполняем следующее:
-                    return (double.NaN);
-
-                ++iteration; // увеличиваем номер итерации
-                t = currentT; // приравниваем обновленное значение t 
-            }
-            while (t >= eps); //цикл выполняется, пока не добьемся нужной точности
-
-            return FuncX;//возвращаем полученное значение функции
+                x = Func(Xn);
+                i++;
+                if (Math.Abs(x - Xn) >= eps && i == 1000)
+                {
+                    error = true;
+                    break;
+                }
+                Xn = x;
+            } while (Math.Abs(Xn - Func(Xn)) > eps);
+            if (error) return Double.NaN;
+            else return x;
         }
     }
 }
